197ce610fa96d8e9de1c1f4c862a2c818e38181e
Mateusgit28 <110639736+Mateusgit28@users.noreply.github.com>
2023-03-01 22:22:52 -0300

    login

---
+++ usuarioinvalido.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    
+
+</head>
+<style>
+    .error{
+        background-image: url(errorr.png);
+        background-repeat: no-repeat;
+    }
+</style>
+<body class="error">
+<div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center row">
+                <div class=" col-md-6 mt-5">
+                    <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                    <p class="lead">
+                        The page you’re looking for doesn’t exist.
+                    </p>
+                    <a href="index.html" class="btn btn-primary">Go Home</a>
+                </div>
+
+            </div>
+        </div>
+</body>
+</html>
